22bc8bec3de167fc248d8bd0c0f4d182c7f4d88f
Mancorl <133156710+Mancorl@users.noreply.github.com>
2023-07-10 23:11:18 +0200

    Add files via upload

---
+++ WindowsApp7/WindowsApp7/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click 'lar meg utløse dette når jeg klikker
+        Dim mass = TextBox1.Text 'tar ut masse
+        Dim velocity = TextBox2.Text 'tar ut hastighet
+        Dim energy = 0.001 * 0.5 * mass * velocity * velocity 'Ganger masse i gram med en halv og hastighet^2 og definerer det som energi
+        MessageBox.Show(energy) 'Viser energien vi får
+    End Sub
+End Class
